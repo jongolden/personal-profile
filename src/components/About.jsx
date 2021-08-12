aeeb2f640b5c87f98e41511c0845ccc5cd2d64d4
Jonathan Golden <jongolden17@gmail.com>
2021-08-11 19:34:47 -0700

    feat(about): update about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => (
     <div className="content larger">
       <p>I'm a software developer based in Phoenix, Arizona.</p>
       <p>My focus has mainly been in building micro-UI React applications, but I'm also skilled in back-end development with Node.js, GraphQL, and Java.</p>
-      <p>When I'm not generating surplus value, I'm providing software solutions for small social justice projects <span role="img" aria-label="fist">✊</span> or putzing around on my bike.</p>
+      <p>When I'm not generating surplus value, I'm providing software solutions for activists and organizing projects <span role="img" aria-label="fist">✊</span> or just putzing around on my bike.</p>
     </div>
   </Section>
 );
